4762ca10f95ecba377a9bfacc2ba3e270a786dc8
Zakinchi <zking@uccs.edu>
2022-11-14 21:40:16 +0000

    Added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "projects#index"
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
