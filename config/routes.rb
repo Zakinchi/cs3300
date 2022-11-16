bae8cd125453a768819e06b25d772b202c0ac6be
Zakinchi <zking@uccs.edu>
2022-11-16 20:18:49 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "projects#index"
-  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
